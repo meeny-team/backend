997bc4dd98887a8345e327030cf7e49e80c268ec
leesangeok <118970618+leesangeok@users.noreply.github.com>
2026-04-28 16:55:14 +0900

    Revert "feat: Crew 도메인 추가 (생성/조회/초대코드 가입/탈퇴/수정)"

--- src/main/java/com/meeny/presentation/crew/dto/JoinByCodeRequest.java
+++
@@ -1,8 +0,0 @@
-package com.meeny.presentation.crew.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record JoinByCodeRequest(
-        @NotBlank(message = "초대 코드는 필수입니다.")
-        String inviteCode
-) {}
